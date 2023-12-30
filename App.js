dcedfafd9a61e3d6fa0b06538d8ba25023909de4
sukanyamd <136873174+sukanyamd@users.noreply.github.com>
2023-12-30 06:29:05 -0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./App.css";
+import Navigation from './components/Navigation'
+import Main from './components/Main'
+
+const App = () => {
+  return (
+    <div>
+      <Navigation/>
+      <Main/>
+    </div>
+  )
+}
+
+export default App
+
