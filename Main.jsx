23b5d0d8f5c2560634f30ba3e8b473cb684ef5da
sukanyamd <136873174+sukanyamd@users.noreply.github.com>
2023-12-30 06:30:18 -0800

    Add files via upload

---
+++ Main.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <main className="main container">
+        <div className="main-content">
+            <h1>FASHION DESERVE THE BEST</h1>
+            <p>
+            YOUR DRESS DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR
+          TASTE.
+            </p>
+       
+        <div className="main-btn">
+            <button>EXPLORE NOW</button>
+          <button className='secondary-btn'>CATEGORY</button>
+        </div>
+        
+        
+    </div>
+    <div className="main-img">
+    <img src="/images/image1.jpg"></img>
+    </div>
+    
+ </main>
+   
+  )
+}
+
+export default Main
---
+++ Navigation.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Navigation = () => {
+  return (
+    <nav className='container'>
+        <div className='logo'>
+            <b>RIVO</b>
+        </div>
+        <ul>
+            <li href="#">HOME</li>
+            <li href="#">SHOP</li>
+            <li href="#">FEATURES</li>
+            <li href="#">CONTACT</li>
+        </ul>
+        <button>
+            Login
+        </button>
+
+      
+      
+   
+    </nav>
+  )
+}
+
+export default Navigation
